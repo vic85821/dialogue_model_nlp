17d55151cc5d0ba3f5739b59c4c705b4a56776a6
Yu Cheng Chang <vic85821@cml25.csie.ntu.edu.tw>
2019-03-31 21:32:03 +0800

    remove the embedding.pkl and add it to the download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
-gdrive download -r 11pyKudt8ilpek46tky7VLw6hyEhUg5Pq
+wget -O ./src/embedding.zip "https://www.dropbox.com/s/5klft0502cjn1tn/embedding.zip?dl=0"
+unzip ./src/embedding.zip -d ./src
+wget -O models.zip "https://www.dropbox.com/s/fshtwvew0ia6mju/models.zip?dl=0"
 unzip models.zip
